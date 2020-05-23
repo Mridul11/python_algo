bb7d911214a9d87603d2971cb51da774ac0409c7
Mridul11 <mridulmishra2991@gmail.com>
2020-05-23 13:08:00 +0530

    added feature a change

--- some.py
+++ some.py
@@ -14,6 +14,8 @@ class Person:
 		@classmethod
 		def classMethodExample(cls):
 			print("Adding class Method")
+			print("Master branch code")
+			print("I am a feature_a change!")
 			print(cls)
 
 
